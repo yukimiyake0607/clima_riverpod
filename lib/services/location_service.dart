1b210285c40be84b4ecc0bf4296903d36746c1e0
yukimiyake0607 <yukimiyake38@gmail.com>
2024-08-01 17:06:35 +0900

    add: 位置情報サービスを作成
    refs 現在地を取得するためのメソッドなど

---
+++ lib/services/location_service.dart
@@ -0,0 +1,29 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:clima_riverpod/models/weather_model.dart';
+
+class LocationService {
+  Future<Location> getCurrentLocation() async {
+    // ユーザーデバイスの位置情報が有効になっているか確認
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      throw Exception('位置情報サービスが無効です');
+    }
+
+    // 拒否されている場合
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        throw Exception('位置情報の権限が拒否されました');
+      }
+    }
+
+    // 永久に拒否されている場合
+    if (permission == LocationPermission.deniedForever) {
+      throw Exception('位置情報の権限が永久に拒否されました。設定から変更してください。');
+    }
+
+    Position position = await Geolocator.getCurrentPosition();
+    return Location(latitude: position.latitude, longitude: position.longitude);
+  }
+}
