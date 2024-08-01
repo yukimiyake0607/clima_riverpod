afbb5d353af25c8afadbe2807824af6f397d4237
yukimiyake0607 <yukimiyake38@gmail.com>
2024-08-01 19:07:48 +0900

    add: weather_serviceを作成
    refs WeatherDataを取得するために必要

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,5 +1,35 @@
+import 'dart:convert';
+import 'package:clima_riverpod/services/location_service.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:clima_riverpod/models/weather_model.dart';
+import 'package:http/http.dart' as http;
 
 class WeatherService {
+  // apiKeyをGitにアップロードしないために難しい書き方をしているだけ
   final String apiKey = dotenv.env['OPENWEATHER_API_KEY'] ?? '';
+
+  Future<WeatherData> getWeatherData(Location location) async {
+    final String url =
+        'https://api.openweathermap.org/data/2.5/onecall?lat=${location.latitude}&lon=${location.longitude}&exclude=minutely,alerts&appid=$apiKey&units=metric';
+
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      print(response.body);
+      final Map<String, dynamic> data = json.decode(response.body);
+      return WeatherData(
+        current: CurrentWeather.fromJson(data['current']),
+        hourly: (data['hourly'] as List)
+            .take(24)
+            .map((e) => HourlyWeather.fromJson(e))
+            .toList(),
+        weekly: (data['daily'] as List)
+            .take(7)
+            .map((e) => WeeklyWeather.fromJson(e))
+            .toList(),
+      );
+    } else {
+      throw Exception('WeatherDataの取得に失敗しました');
+    }
+  }
 }
