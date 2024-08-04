ff8ab1073ae47fa1247333a63665fbae877a0452
yukimiyake0607 <yukimiyake38@gmail.com>
2024-08-04 17:49:14 +0900

    add: forecast_weatherに天気情報を取得し、表示させる
    refs _getWeatherIconでweatherIdに応じてアイコンを変化させる

--- lib/widgets/forecast_weather.dart
+++ lib/widgets/forecast_weather.dart
@@ -6,6 +6,28 @@ class ForecastWeatherWidget extends StatelessWidget {
   final List<ForecastWeather> forecastWeather;
   const ForecastWeatherWidget({super.key, required this.forecastWeather});
 
+  Icon _getWeatherIcon(int weatherId) {
+    if (weatherId >= 200 && weatherId < 300) {
+      return const Icon(Icons.thunderstorm, color: Colors.yellow);
+    }
+    if (weatherId >= 300 && weatherId < 400) {
+      return const Icon(Icons.grain, color: Colors.white);
+    }
+    if (weatherId >= 400 && weatherId < 500) {
+      return const Icon(Icons.umbrella, color: Colors.lightBlueAccent);
+    }
+    if (weatherId >= 500 && weatherId < 600) {
+      return const Icon(Icons.ac_unit, color: Colors.white);
+    }
+    if (weatherId >= 600 && weatherId < 700) {
+      return const Icon(Icons.cloud, color: kGreyWhiteMidColor);
+    }
+    if (weatherId == 800) {
+      return const Icon(Icons.wb_sunny, color: Colors.yellow);
+    }
+    return const Icon(Icons.cloud, color: kGreyWhiteMidColor);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -40,6 +62,8 @@ class ForecastWeatherWidget extends StatelessWidget {
               final now = DateTime.now();
               final forecastTime = now.add(Duration(hours: 3 * index));
               final timeString = index == 0 ? '現在' : '${forecastTime.hour}時';
+
+              final forecastWeatherData = forecastWeather[index];
               return Container(
                 padding: const EdgeInsets.symmetric(vertical: 8),
                 decoration: BoxDecoration(
@@ -52,7 +76,7 @@ class ForecastWeatherWidget extends StatelessWidget {
                 child: Row(
                   children: [
                     SizedBox(
-                      width: 50,
+                      width: 55,
                       child: Text(
                         timeString,
                         style: kForecastWeather,
@@ -60,35 +84,38 @@ class ForecastWeatherWidget extends StatelessWidget {
                       ),
                     ),
                     const SizedBox(width: 24),
-                    Icon(
-                      Icons.sunny,
-                      color: Colors.yellow,
-                    ),
+                    _getWeatherIcon(forecastWeatherData.weatherId),
                     const Spacer(),
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.thermostat,
-                          color: Colors.white,
-                        ),
-                        Text(
-                          '27°',
-                          style: kForecastTemperature,
-                        ),
-                      ],
+                    SizedBox(
+                      width: 60,
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.thermostat,
+                            color: Colors.white,
+                          ),
+                          Text(
+                            '${forecastWeatherData.temperature.toInt()}°',
+                            style: kForecastTemperature,
+                          ),
+                        ],
+                      ),
                     ),
                     const SizedBox(width: 16),
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.opacity,
-                          color: Colors.lightBlueAccent,
-                        ),
-                        Text(
-                          '60%',
-                          style: kForecastTemperature,
-                        ),
-                      ],
+                    SizedBox(
+                      width: 70,
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.opacity,
+                            color: Colors.lightBlueAccent,
+                          ),
+                          Text(
+                            '${forecastWeatherData.humidity}%',
+                            style: kForecastTemperature,
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
